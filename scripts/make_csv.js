845c67e147a7c340bd5e7d0d30369ca6cee0ebdb
ngoduyanh <ngoduyanh.chip@gmail.com>
2022-02-08 12:02:50 +0700

    everything gone lmfao

--- scripts/make_csv.js
+++ scripts/make_csv.js
@@ -2,29 +2,21 @@
 const labels = [
     "ID",
     "Title",
-    "Overall",
-    "Emotion",
-    "Meme",
-    "Music",
-    "Additional"
+    "Overall"
 ];
 
 const output = entry => {
     return [
         entry.id,
         entry.title,
-        entry.score.value,
-        entry.score.emotion.value,
-        entry.score.meme.value,
-        entry.score.music.value,
-        entry.score.additional.value
+        entry.score.overall,
     ];
 };
 
 // Script content
 const fs = require("fs");
 const csv = require("csv");
-const data = JSON.parse(fs.readFileSync("impl/nrs.json"));
+const data = JSON.parse(fs.readFileSync("impl/nrs_2.0.json"));
 let output_data = [labels];
 for(const entry of data.entries) {
     output_data.push(output(entry));
